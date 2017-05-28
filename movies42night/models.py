36adb20e1549e512a7b691da14b5e33f3771e3ed
programistka <iwona.lalik@gmail.com>
2017-05-28 12:09:24 +0200

    Fixture

--- movies42night/models.py
+++ movies42night/models.py
@@ -3,10 +3,13 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=1000)
-
+    
     def __str__(self):
         return self.title
 
 
 class Status(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
